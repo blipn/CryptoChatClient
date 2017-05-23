6d898fa04fe795b052fdc277c485d58da6de434c
Unknown <oblipn@gmail.com>
2017-05-23 22:46:11 +0200

    base

---
+++ src/CryptoChatClient/Controller.java
@@ -0,0 +1,14 @@
+package CryptoChatClient;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class Controller {
+
+    @FXML Label label1;
+
+    public void initialize(){
+
+    }
+
+}
---
+++ src/CryptoChatClient/Main.java
@@ -0,0 +1,24 @@
+package CryptoChatClient;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Menu");
+        //primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
